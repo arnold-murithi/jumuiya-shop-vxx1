27542f876846dcd3c47ebcbf47d70f8a5692868e
arnold-murithi <arnoldmurithi132@gmail.com>
2024-12-04 12:39:28 +0300

    dashboard fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import dashboardImage from "@/public/interior.jpg"
 import { getFiveProducts } from "../data-access/product";
+import { Button } from "@/components/ui/button";
 
 
 export default async function Home() {
@@ -14,10 +15,13 @@ export default async function Home() {
       <div className="ml-5 ">
         <h1 className="text-white text-9xl relative font-bold p-40">ONLINE STORE</h1>
         </div>
+        <div className="place-items-center mt-0">
+            <h1 className="font-mono font-bold text-4xl">Discounted Products</h1>
+          </div>
         <div className="min-h-64 ">
-          <div className="flex mx-24 ms-0.5 me-0 bg-fuchsia-700">
+          <div className="flex mx-24 ms-0.5 me-0 bg-fuchsia-200">
             {fiveProducts.map((product) =>(
-              <div key={product.id} className="items-center justify-center shadow-xl border-black border-1 w-64 h-44 mt-10 ml-3 rounded-xl p-4">
+              <div key={product.id} className="place-items-center justify-center shadow-xl border-black border-1 w-64 h-44 mt-10 ml-3 rounded-xl p-4">
                 <Image 
                 src={product.imagePath}
                 width={100}
@@ -25,6 +29,7 @@ export default async function Home() {
                 alt={product.description}
                 />
               <h2 className="mt-8 font-semibold">{product.name}</h2>
+              <Button className="w-full">Buy now</Button>
             </div>
             ))}
           </div>
