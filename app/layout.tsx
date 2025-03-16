5d82627ad25a7f982102c50287bf0744de484056
arnold-murithi <arnoldmurithi132@gmail.com>
2025-03-16 22:44:54 +0300

    Remove dead code

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  auth,
 }: Readonly<{
   children: React.ReactNode;
-  auth: React.ReactNode;
 }>) {
   
   return (
