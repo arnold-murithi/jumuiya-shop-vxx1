4825d0dba60d6d9bb8fdb5dd9fa99671de3a1262
arnold-murithi <arnoldmurithi132@gmail.com>
2025-04-06 09:26:11 +0300

    Minor change

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -2,9 +2,6 @@ import { Product } from "@prisma/client";
 import { create } from "zustand"
 import { devtools, persist } from "zustand/middleware"
 
-// type CartItem = Product & {
-//     quantity: number;
-// }
 
 type CartState = {
     cart: Product[];
@@ -22,24 +19,14 @@ export const useProductStore = create<CartState>()(
             }),
             {
                 name: 'cart-storage',
-                // version: 1,
-                // migrate: (persistedState, version) => {
-                //     if (version === 1) {
-                //         return { cart: [] }
-                //     }
-                //     return persistedState
-                // }
+                version: 1,
+                migrate: (persistedState, version) => {
+                    if (version === 1) {
+                        return { cart: [] }
+                    }
+                    return persistedState
+                }
             },
         )
     )
 )
-// addToCart: (product) => set((state) => {
-//     const existingItem = state.cart.findIndex((item) => item.id === product.id)
-//     if (existingItem !== -1) {
-//         const updatedCart = state.cart.map((item, index) =>
-//             index === existingItem ? { ...item, quantity: item.quantity + 1 } : item
-//         )
-//         return { cart: updatedCart }
-//     }
-//     return { cart: [...state.cart, { ...product, quantity: 1 }] }
-// }),
